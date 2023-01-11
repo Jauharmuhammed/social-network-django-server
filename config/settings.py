a2df140a33cb5d382a3ef6092e9f6ebbe9332c61
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2023-01-11 21:15:17 +0530

    allowed hosts changed

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['showyourwork.up.railway.app', '127.0.0.1']
 
 
 # Application definition
