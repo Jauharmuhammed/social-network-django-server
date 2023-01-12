de2eb0d862020bf0257b38c9f880afe396aca36f
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2023-01-12 21:59:01 +0530

    asgi updated

--- config/asgi.py
+++ config/asgi.py
@@ -14,13 +14,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
 from apps.chats.middleware import JwtAuthMiddlewareStack
-from django.core.asgi import get_asgi_application
 import django
 
 # If DJANGO_SETTINGS_MODULE is unset, default to the local settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-django.setup()
 # This application object is used by any ASGI server configured to use this file.
 django_application = get_asgi_application()
 
@@ -30,7 +28,7 @@ from . import routing  # noqa isort:skip
 from channels.routing import ProtocolTypeRouter, URLRouter  # noqa isort:skip
 from apps.chats.middleware import JwtAuthMiddlewareStack 
 
-
+django.setup()
 application = ProtocolTypeRouter(
     {
         "http": django_application,
