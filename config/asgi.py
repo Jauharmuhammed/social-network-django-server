9189822f15e500bed2cd3675f15473f3b32a314a
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2023-01-13 17:51:28 +0530

    updates

--- config/asgi.py
+++ config/asgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/asgi/
 
 
 import os
+import django
 
 # If DJANGO_SETTINGS_MODULE is unset, default to the local settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
@@ -20,7 +21,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
 from apps.chats.middleware import JwtAuthMiddlewareStack
-import django
 
 
 # This application object is used by any ASGI server configured to use this file.
