4e124433cd25e66328394773bb07fa60de94a083
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2023-01-16 18:20:49 +0530

    settings updated

--- config/routing.py
+++ config/routing.py
@@ -3,8 +3,8 @@ from apps.chats.consumers import ChatConsumer, ChatNotificationConsumer
 from apps.notifications.consumers import NotificationsConsumer
 
 websocket_urlpatterns = [
-    path("chats/<conversation_name>/", ChatConsumer.as_asgi()),
-    path("notifications/chat/", ChatNotificationConsumer.as_asgi()),
+    path("ws/chats/<conversation_name>/", ChatConsumer.as_asgi()),
+    path("ws/notifications/chat/", ChatNotificationConsumer.as_asgi()),
 
-    path("notifications/", NotificationsConsumer.as_asgi()),
+    path("ws/notifications/", NotificationsConsumer.as_asgi()),
 ]
