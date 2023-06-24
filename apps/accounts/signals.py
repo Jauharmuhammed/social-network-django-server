446da4a832c29d0d6295099c075d10d2ce6f594d
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2023-06-24 22:27:54 +0530

    profile creation signal is error handled

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -18,16 +18,13 @@ def create_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=CustomUser)
 def create_profile(sender, instance, created, **kwargs):
-    if created == False:
-        try:
-            print('hiii', instance)
+    try:
+        if created == False:
             profile = UserProfile.objects.filter(user=instance).first()
             profile.first_name = instance.first_name
             profile.last_name = instance.last_name
             profile.last_name = instance.last_name
             profile.username = instance.username
             profile.save()
-
-            print('Profile updated!')
-        except Exception as e:
-            print(e)
+    except:
+            pass
